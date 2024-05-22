79f6f8d722fa6258407f880ea673ec9fe1f23911
Stephen <hellostephenwm@gmail.com>
2024-05-22 15:05:45 -0400

    invoked notification component in toggler catch method

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import Note from "./components/Note";
+import Notification from "./components/Notification";
 import noteService from "./services/notes";
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState("a new note...");
 	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	useEffect(() => {
 		noteService.getAll().then((initialNotes) => {
@@ -27,7 +29,12 @@ const App = () => {
 				setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
 			})
 			.catch((error) => {
-				alert(`the note '${note.content}' was already deleted from the server`);
+				setErrorMessage(
+					`Note '${note.content}' was already removed from the server`
+				);
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 5000);
 				setNotes(notes.filter((n) => n.id !== id));
 			});
 	};
@@ -51,6 +58,8 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+
+			<Notification message={errorMessage} />
 
 			<button onClick={() => setShowAll(!showAll)}>
 				show {showAll ? "important" : "all"}
