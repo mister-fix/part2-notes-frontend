adbee3cca95400de45487b1d718e0ec93aaaed2a
Stephen <hellostephenwm@gmail.com>
2024-05-22 14:06:35 -0400

    rendering notes passed as props in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-const App = () => {
-	return <div>App</div>;
+const App = (props) => {
+	const { notes } = props;
+
+	return (
+		<div>
+			<h1>Notes</h1>
+
+			<ul>
+				<li>{notes[0].content}</li>
+				<li>{notes[1].content}</li>
+				<li>{notes[2].content}</li>
+			</ul>
+		</div>
+	);
 };
 
 export default App;
