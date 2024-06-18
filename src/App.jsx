1cdbee93d86494c3750b1b4dc0c31c2ce0bf7af5
Stephen <hellostephenwm@gmail.com>
2024-06-17 20:15:29 -0400

    setting authorization token

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,8 @@ const App = () => {
 
 		try {
 			const user = await loginService.login({ username, password });
+
+			noteService.setToken(user.token);
 			setUser(user);
 			setUsername("");
 			setPassword("");
