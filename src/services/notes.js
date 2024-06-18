ba6c2cd93d7ec1e6f87206618aae1ded5e3e6dbb
Stephen <hellostephenwm@gmail.com>
2024-06-17 20:16:47 -0400

    added token initialization, updated create method to pass auth headers

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,14 +1,25 @@
 import axios from "axios";
-
 const baseUrl = "api/notes";
+
+let token = null;
+
+const setToken = (newToken) => {
+	token = `Bearer ${newToken}`;
+};
 
 const getAll = () => {
 	const request = axios.get(baseUrl);
 	return request.then((response) => response.data);
 };
 
-const create = (newObject) => {
-	const request = axios.post(baseUrl, newObject);
+const create = async (newObject) => {
+	const config = {
+		headers: {
+			Authorization: token,
+		},
+	};
+
+	const request = axios.post(baseUrl, newObject, config);
 	return request.then((response) => response.data);
 };
 
@@ -21,4 +32,5 @@ export default {
 	getAll,
 	create,
 	update,
+	setToken,
 };
