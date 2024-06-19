3cc32828aa8f26303ff6e5e2ff5fbfb0c76b71ee
Stephen <hellostephenwm@gmail.com>
2024-06-19 14:04:37 -0400

    exported note form to own component

---
+++ src/components/NoteForm.jsx
@@ -0,0 +1,17 @@
+const NoteForm = ({ onSubmit, value, handleChange }) => {
+	return (
+		<div>
+			<h2>Create a new note</h2>
+
+			<form onSubmit={onSubmit}>
+				<input
+					value={value}
+					onChange={handleChange}
+				/>
+				<button type="submit">save</button>
+			</form>
+		</div>
+	);
+};
+
+export default NoteForm;
