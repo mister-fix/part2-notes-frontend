328d1f09a47380c2d028370e5fafbd7ddf53b981
Stephen <hellostephenwm@gmail.com>
2024-06-26 14:28:05 -0400

    tests for the NoteForm component

--- src/components/NoteForm.test.jsx
+++ src/components/NoteForm.test.jsx
@@ -1,19 +1,19 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import NoteForm from "./NoteForm";
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import NoteForm from './NoteForm'
 
-test("<NoteForm /> updates parent state andn calls onSubmit", async () => {
-	const createNote = vi.fn();
-	const user = userEvent.setup();
+test('<NoteForm /> updates parent state andn calls onSubmit', async () => {
+  const createNote = vi.fn()
+  const user = userEvent.setup()
 
-	render(<NoteForm createNote={createNote} />);
+  render(<NoteForm createNote={createNote} />)
 
-	const input = screen.getByRole("textbox");
-	const sendButton = screen.getByText("save");
+  const input = screen.getByRole('textbox')
+  const sendButton = screen.getByText('save')
 
-	await user.type(input, "testing a form...");
-	await user.click(sendButton);
+  await user.type(input, 'testing a form...')
+  await user.click(sendButton)
 
-	expect(createNote.mock.calls).toHaveLength(1);
-	expect(createNote.mock.calls[0][0].content).toBe("testing a form...");
-});
+  expect(createNote.mock.calls).toHaveLength(1)
+  expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
+})
